029bd4f0071496d953dcd05e2c9bd0d86d47e6f5
Ainsx <eorms9762@gmail.com>
2025-11-05 01:24:40 +0000

    cart function not working

--- public/script.js
+++ public/script.js
@@ -4,6 +4,22 @@ const contactForm= document.getElementById("contactform");
 if(contactForm){
     contactForm.addEventListener("submit", (e) => {
         e.preventDefault();
+        document.addEventListener('DOMContentLoaded', () => updateCartEmptyState());
+
+    const first = document.getElementById("first-name").value;
+    const last = document.getElementById("last-name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if(!first || !last|| !email || !message){
+        alert ("Please fill in all fields");
+        return;
+    }
+
+    alert(`Thank you, ${first}! Your messasge has been received.`);
+    contactForm.reset(); 
+});
+}
 
 function getCart() {
     try {
@@ -69,23 +85,25 @@ document.addEventListener('click', (e) => {
     }
 });
 
-// initialize cart UI on page load
-document.addEventListener('DOMContentLoaded', () => updateCartEmptyState());
 
-    const first = document.getElementById("first-name").value;
-    const last = document.getElementById("last-name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
 
-    if(!first || !last|| !email || !message){
-        alert ("Please fill in all fields");
-        return;
-    }
 
-    alert(`Thank you, ${first}! Your messasge has been received.`);
-    contactForm.reset(); 
-});
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Login Page: Sign Up / Login Form
 const signupForm= document.getElementById("signup-form");
 
